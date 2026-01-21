fc36f17789d78846074f3399f064dd43e5cd7610
eujincho1 <yujinjo1@naver.com>
2026-01-21 12:13:12 +0900

    Fix: resolve type error in DeleteInvoice action

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -30,7 +30,9 @@ export function DeleteInvoice({ id }: { id: string }) {
   const deleteInvoiceWithId = deleteInvoice.bind(null, id);
 
   return (
-    <form action={deleteInvoiceWithId}>
+   <form action={async (formData) => {
+      await deleteInvoiceWithId(formData);
+    }}>
 
     
       <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
